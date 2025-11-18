1376ddc6908edbd115e6bdc6f41e84e1f2e94a34
Dago22 <85245738+Dago22@users.noreply.github.com>
2025-11-18 02:53:13 -0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,13 @@
-const mongoose = require('mongoose');
-const connectDB = async (uri) => {
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  console.log('MongoDB connected');
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected:", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
-module.exports = connectDB;
+
+export default connectDB;
